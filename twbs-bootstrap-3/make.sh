1fb804dc3c605ede5823b57d01d29aa7558705c3
Vino Rodrigues <github@m.vinorodrigues.com>
2016-04-18 11:47:33 +1000

    bs3 fix to test-page

--- twbs-bootstrap-3/make.sh
+++ twbs-bootstrap-3/make.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 lessc less/bootstrap-pr.less --source-map="css/bootstrap-pr.map" > css/bootstrap-pr.css
 lessc less/bootstrap-pr.less --clean-css="--s1 --advanced --compatibility=ie8" > css/bootstrap-pr.min.css
+echo Done.
